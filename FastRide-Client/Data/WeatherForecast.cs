0ab07bcfa21b25bb16b8b59e33f7d435c0dea103
DizzYForester <dizzyforester@gmail.com>
2024-10-12 18:21:56 +0300

    create projects

---
+++ FastRide-Client/Data/WeatherForecast.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace FastRide_Client.Data;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
---
+++ FastRide-Server/Function1.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask;
+using Microsoft.Extensions.Logging;
+
+namespace FastRide_Server
+{
+    public static class Function1
+    {
+        [Function("Function1")]
+        public static async Task<List<string>> RunOrchestrator(
+            [OrchestrationTrigger] TaskOrchestrationContext context)
+        {
+            var outputs = new List<string>();
+
+            // Replace "hello" with the name of your Durable Activity Function.
+            outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "London"));
+
+            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+            return outputs;
+        }
+
+        [Function(nameof(SayHello))]
+        public static string SayHello([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation("Saying hello to {name}.", name);
+            return $"Hello {name}!";
+        }
+
+        [Function("Function1_HttpStart")]
+        public static async Task<HttpResponseMessage> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
+            [DurableClient] IDurableOrchestrationClient starter,
+            ILogger log)
+        {
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync("Function1", null);
+
+            log.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+}
