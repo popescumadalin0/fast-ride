eb7b1fabdd3a085f39d2a9c88d5426c96525efaa
DizzYForester <dizzyforester@gmail.com>
2024-11-19 00:24:23 +0200

    try make it work

--- FastRide.Client/FastRide.Client/Pages/Counter.razor.cs
+++ FastRide.Client/FastRide.Client/Pages/Counter.razor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using FastRide.Server.Sdk.Contracts;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 namespace FastRide.Client.Pages;
 
@@ -10,10 +11,20 @@ public partial class Counter
 
     [Inject] private IFastRideApiClient FastRideApiClient { get; set; }
 
+    [Inject] private IAccessTokenProvider TokenProvider { get; set; }
+
     private async Task IncrementCount()
     {
-        var test = await FastRideApiClient.GetUserTypeAsync("test", "test");
+        var state = await TokenProvider.RequestAccessToken();
+        state.TryGetToken(out var token);
 
+        if (state.Status == AccessTokenResultStatus.Success)
+        {
+            currentCount = -10;
+        }
+
+        var a = token;
+        var test = await FastRideApiClient.GetUserTypeAsync("test", "test");
         currentCount++;
     }
 }
