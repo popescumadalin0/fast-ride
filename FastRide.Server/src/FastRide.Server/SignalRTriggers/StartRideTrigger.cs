1989762a32756265ca0199b57b07864c05d8143a
DizzYForester <dizzyforester@gmail.com>
2024-12-10 00:56:46 +0200

    fix signalR emulator

--- FastRide.Server/src/FastRide.Server/SignalRTriggers/StartRideTrigger.cs
+++ FastRide.Server/src/FastRide.Server/SignalRTriggers/StartRideTrigger.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Threading.Tasks;
+using FastRide.Server.Authentication;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -15,10 +16,10 @@ public class StartRideTrigger
         _logger = logger;
     }
 
-    /*[Function(nameof(StartRideTrigger))]
-    [SignalROutput(HubName = "ride",  ConnectionStringSetting = "SignalRConnection")]
+    [Function(nameof(StartRideTrigger))]
+    [SignalROutput(HubName = "serverless")]
     public Task HttpStart(
-        [SignalRTrigger("ride", "command", "start-ride", ConnectionStringSetting = "SignalRConnection")]
+        [SignalRTrigger("serverless", "messages", "start-ride")]
         SignalRInvocationContext invocationContext, FunctionContext functionContext)
     {
         // Function input comes from the request content.
@@ -31,13 +32,14 @@ public class StartRideTrigger
             Arguments = new[] { message }
         };
         return await starter.CreateCheckStatusResponseAsync(req, instanceId);#1#
-
+*/
         return Task.CompletedTask;
-    }*/
-    
+    }
+
     [Function("negotiate")]
     public static HttpResponseData Negotiate([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequestData req,
-        [SignalRConnectionInfoInput] string connectionInfo)
+        [SignalRConnectionInfoInput(HubName = "serverless")]
+        string connectionInfo)
     {
         var response = req.CreateResponse(HttpStatusCode.OK);
         response.Headers.Add("Content-Type", "application/json");
--- README.md
+++ README.md
@@ -1,17 +1,6 @@
 # fast-ride
-
 Application for ride sharing
-<br/>
+
 **Setup:**
-<br/>
-_For Azurite-emulator instalation:_
-<br/>
-1. docker run -p 10000:10000 -p 10001:10001 -p 10002:10002 mcr.microsoft.com/azure-storage/azurite
-<br/>
-_For signalR-emulator instalation:_
-<br/>
-1. cd ...\fast-ride\Docker
-<br/>
-2. docker build -t signalr-emulator -f signalr-emulator .
-<br/>
-3. docker-compose -f signalr-docker-compose.yml up -d
+
+Run: docker-compose -f signalr-docker-compose.yml up -d
