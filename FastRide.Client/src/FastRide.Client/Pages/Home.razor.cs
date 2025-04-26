005c947edd5c2aa64caa8d2247cc187c9cebd4d8
Madalin Popescu <dizzyforester@gmail.com>
2025-04-26 22:17:16 +0300

    fix icons

--- FastRide.Client/src/FastRide.Client/Pages/Home.razor.cs
+++ FastRide.Client/src/FastRide.Client/Pages/Home.razor.cs
@@ -138,16 +138,16 @@ public partial class Home : ComponentBase, IDisposable
     private static Task PredefineMapContentAsync(RealTimeMap realTimeMap)
     {
         realTimeMap.Geometric.Points.Appearance(item => item.type == "current").pattern =
-            new RealTimeMap.PointIcon() { iconUrl = $"icons/currentCar.png", iconSize = [40, 40] };
+            new RealTimeMap.PointIcon() { iconUrl = $"icons/currentCar.png", iconSize = [40, 40], iconAnchor = [20,40] };
 
         realTimeMap.Geometric.Points.Appearance(item => item.type == "pin").pattern =
-            new RealTimeMap.PointIcon() { iconUrl = $"icons/pin.png", iconSize = [40, 40] };
+            new RealTimeMap.PointIcon() { iconUrl = $"icons/pin.png", iconSize = [40, 40], iconAnchor = [20,40]};
 
         realTimeMap.Geometric.Points.Appearance(item => item.type == "human").pattern =
-            new RealTimeMap.PointIcon() { iconUrl = $"icons/human.png", iconSize = [40, 40] };
+            new RealTimeMap.PointIcon() { iconUrl = $"icons/human.png", iconSize = [40, 40], iconAnchor = [20,40] };
 
         realTimeMap.Geometric.Points.Appearance(item => item.type == "driver").pattern =
-            new RealTimeMap.PointIcon() { iconUrl = $"icons/driver.png", iconSize = [40, 40] };
+            new RealTimeMap.PointIcon() { iconUrl = $"icons/driver.png", iconSize = [40, 40], iconAnchor = [20,40] };
 
         return Task.CompletedTask;
     }
