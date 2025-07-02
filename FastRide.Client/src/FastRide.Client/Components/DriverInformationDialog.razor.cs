0108357f97c82879a8b8992fc6b974a66506f3b8
madalin-popescu <m.popescu@flanderijn.nl>
2025-07-02 12:13:51 +0300

    same for driver details

--- FastRide.Client/src/FastRide.Client/Components/DriverInformationDialog.razor.cs
+++ FastRide.Client/src/FastRide.Client/Components/DriverInformationDialog.razor.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using FastRide.Client.Contracts;
 using FastRide.Server.Contracts.Enums;
@@ -34,6 +36,30 @@ public partial class DriverInformationDialog : IDisposable
         }
 
         _user = user.Response;
+        
+        var httpClient = new HttpClient();
+
+        var url = _user.PictureUrl;
+
+        using var requestMessage =
+            new HttpRequestMessage(HttpMethod.Get, url);
+        requestMessage.Headers.Accept.ParseAdd("*/*");
+        requestMessage.Headers.AcceptEncoding.ParseAdd("gzip, deflate, br");
+        requestMessage.Headers.UserAgent.ParseAdd("PostmanRuntime/7.44.1");
+
+        var response = await httpClient.SendAsync(requestMessage);
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+
+            var bytes = await response.Content.ReadAsByteArrayAsync();
+            _user.PictureUrl =
+                $"data:image/jpg;base64, {Convert.ToBase64String(bytes)}";
+        }
+        else
+        {
+            _user.PictureUrl = "default-image.png";
+        }
     }
 
     public void Dispose()
